d6c44e93f6551dae648e74ec29d1a676f115553c
xianweisen <1298267898@qq.com>
2019-04-09 10:07:56 +0800

    增加重启目标app的功能

--- app/src/main/java/com/weisente/demo/MainActivity.java
+++ app/src/main/java/com/weisente/demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.weisente.demo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.weisente.demo.base.Constants;
+import com.weisente.demo.util.ShellUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ShellUtil.execCommand("am force-stop " +Constants.TARGET_PACKAGE_NAME,true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ShellUtil.execCommand("am start " + "com.zhihu.android/.app.ui.activity.LauncherActivity",true);
+            }
+        },100);
+
+
     }
 }
